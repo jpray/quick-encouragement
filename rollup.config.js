1c82178ef4a1f8866c8ce29e27fea0d92ed342ce
John Pray <jdpray@gmail.com>
2019-04-04 20:27:31 -0400

    mvp

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import typescript from 'rollup-plugin-typescript';
 import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import postcss from 'rollup-plugin-postcss'
+import json from 'rollup-plugin-json';
 
 export default {
   input: 'src/app.ts',
@@ -10,6 +11,26 @@ export default {
     format: 'iife'
   },
   plugins: [
+    json({
+      // All JSON files will be parsed by default,
+      // but you can also specifically include/exclude files
+      // include: 'node_modules/**',
+      // exclude: [ 'node_modules/foo/**', 'node_modules/bar/**' ],
+
+      // for tree-shaking, properties will be declared as
+      // variables, using either `var` or `const`
+      //preferConst: true, // Default: false
+
+      // specify indentation for the generated default export —
+      // defaults to '\t'
+      //indent: '  ',
+
+      // ignores indent and generates the smallest code
+      //compact: true, // Default: false
+
+      // generate a named export for every property of the JSON object
+      //namedExports: true // Default: true
+    }),
     postcss({
         plugins: []
     }),
